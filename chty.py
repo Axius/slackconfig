766db071555f7b76556bdf4684adc29ec5f77baf
florin <florin89z@gmail.com>
2010-03-13 22:06:00 +0200

    new file:   chty.py 	new file:   compare.py  comparasion operators in python.

---
+++ chty.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+a = 24            
+b = float(a)  #this will change an int to float
+print a
+print b
+
+#
+#
+c = 38.0
+d = int(c) #this will change a float to an int
+print c
+print d
+
+#
+#
+a = '76.3'
+b = float(a) #this will change the string to a float
+print a
+print b
+
+
+y = 34 
+if y < 33:   #one can you use < or <= LESS THAN  comparison operators 
+    print "this true"
+else:
+    print "this fucking false"
+
+#
+#
+x = 24
+if x > 12: #For comparison one can use > or >= GREATER THAN
+    print "this hell on earth"
+else:
+    print "this is firehello"
+
+#
+#
+z = 12
+if z != 12: #for "NOT EQUAL TO" one can you != or <> 
+    print "this true"
+else:
+    print "this is false"
+
+#
+#
+q = 42
+if q >= 42: #for EQUAL TO one can you use >= or <=
+    print "this is awesome"
+else:
+    print "good"
+
+#
+#
+e = 68
+if e == 68: #for checking if two this are equal one can use == 
+    print "this a good day"
+else:
+    print "this is a fucking day"
+    
---
+++ compare.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+num1 = float(raw_input("Enter the first number: ")) 
+num2 = float(raw_input("Enter the first number: "))
+if num1 < num2:
+    print num1, "is less than", num2
+if num1 > num2:
+    print num1, "is greater than", num2
+if num1 == num2:
+    print num1, "is equal to", num2
+if num1 != num2:
+    print num1, "is not equal to", num2
+
